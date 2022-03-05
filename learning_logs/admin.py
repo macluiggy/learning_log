d8f4c62fd62544327a5c1648655dee25483fde3f
luiggy macias <ferrinluiggy@gmail.com>
2022-03-05 10:47:32 -0500

    adding topics into the django admin site

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Topic # el punto . es para importar el modelo desde el mismo directorio que el archivo admin.py, esto es como ./ para importar un JavaScript
+
+admin.site.register(Topic) # registra el modelo Topic a traves del administrador de Django
