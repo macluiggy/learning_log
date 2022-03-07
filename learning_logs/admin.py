1184df16e3a97aa2b8b42f75ca756dba659b66e4
luiggy macias <ferrinluiggy@gmail.com>
2022-03-07 10:27:52 -0500

    adding entries

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Topic # el punto . es para importar el modelo desde el mismo directorio que el archivo admin.py, esto es como ./ para importar un JavaScript
+from .models import Entry, Topic # el punto . es para importar el modelo desde el mismo directorio que el archivo admin.py, esto es como ./ para importar un JavaScript
 
 admin.site.register(Topic) # registra el modelo Topic a traves del administrador de Django
+admin.site.register(Entry) # registra el modelo Entry a traves del administrador de Django
