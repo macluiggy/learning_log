e2742ce0ac1324d061d8066991f24d86191dfd70
luiggy macias <ferrinluiggy@gmail.com>
2022-03-09 16:32:15 -0500

    adding routes for a specific topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,4 +6,7 @@ app_name = 'learning_logs' # ayuda a distinguir este archivo urls.py de otro en 
 urlpatterns = [ # lista de paginas individuales que pueden ser accedidas desde la app learning_logs
   # Home page.
   path('', views.index, name='index'), # el primer argumento una string vacia, es la url que se va a usar para acceder a esta pagina, es como router.route('/') en express, djando recive la url de peticion y  trata de enrutar esa ruta a una view, esto lo hace mediante la busqueda de la url que encaje con la de la peticion del cliente, si no encuentra ninguna url que coincida con la peticion del cliente, entonces se envia una respuesta de error 404, django ignora la base url, es decir que la string vacia encaja con la base url. El segundo argumento es la funcion que se va a ejecutar cuando se acceda a la url, esto es como router.route(/).get(views.index) en express. El tercer argumento es simplemente un nombre de referencia, esto es para poder referenciarlo en otras secciones de codigo en lugar de escribit la url.
+  # Page that shows all topics.
+  path('topics', views.topics, name='topics'),
+  path('topics/<int:topic_id>', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
 def index(request):
-  """The home page for Learning Log"""
-  return render(request, 'learning_logs/index.html') # render toma dos argumentos, el request y el nombre del template, esta vela por la carpeta base: templates, entoces el path real es: __dirname/learning_logs/templates/learning_logs/index.html
+    """The home page for Learning Log"""
+    return render(request, 'learning_logs/index.html') # render toma dos argumentos, el request y el nombre del template, esta vela por la carpeta base: templates, entoces el path real es: __dirname/learning_logs/templates/learning_logs/index.html
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    # print(topics.all())
+    context = {'topics': topics} # context es un diccionario que se va a pasar a la plantilla para que pueda ser usado en la plantilla, las keys son los nombres de las variables que se van a usar en la plantilla, en este caso topics, y los values son los valores son los datos que necesitamos pasar a la plantilla.
+    return render(request, 'learning_logs/topics.html', context) # render toma dos argumentos, el request y el nombre del template, esta vela por la carpeta base: templates, entoces el path real es: __dirname/learning_logs/templates/learning_logs/topics.html
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries."""
+    topic = Topic.objects.get(id=topic_id)
+    print(topic)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
