666110c04448317829b3d145617e691958e235dd
luiggy macias <ferrinluiggy@gmail.com>
2022-03-09 13:47:14 -0500

    learning to add templates

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+  """The home page for Learning Log"""
+  return render(request, 'learning_logs/index.html') # render toma dos argumentos, el request y el nombre del template, esta vela por la carpeta base: templates, entoces el path real es: __dirname/learning_logs/templates/learning_logs/index.html
--- pizzas/models.py
+++ pizzas/models.py
@@ -14,3 +14,6 @@ class Topping(models.Model):
 
   def __str__(self):
       return self.name
+
+
+# from 
