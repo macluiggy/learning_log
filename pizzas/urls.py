666b0b24e92de2e7ae00945292372142b30efaab
luiggy macias <ferrinluiggy@gmail.com>
2022-03-11 10:05:10 -0500

    chapter 18 finished

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'pizzas'
 
 urlpatterns = [
-    path('pizzas', views.index, name='index'),
+    path('home_pizzas', views.index, name='home_pizzas'),
+    path('pizzas', views.pizzas, name='pizzas'),
+    path('pizzas/<int:pizza_id>', views.pizza, name='pizza'),
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-
+from .models import Pizza
 # Create your views here.
 
 def index(req):
   """The home page for Learning Log"""
   return render(req, 'pizzas/index.html')
+
+def pizzas(req):
+  """Show all pizzas"""
+  pizzas = Pizza.objects.all()
+  context = {'pizzas': pizzas}
+  return render(req, 'pizzas/pizzas.html', context)
+
+def pizza(req, pizza_id):
+  """Show a single pizza and all its toppings"""
+  pizza = Pizza.objects.get(id=pizza_id)
+  toppings = pizza.topping_set.all()
+  context = {'pizza': pizza, 'toppings': toppings}
+  
+  return render(req, 'pizzas/pizza.html', context)
