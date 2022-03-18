27bc55fd8068663c25b878d63a7f37eb6308cc95
luiggy macias <ferrinluiggy@gmail.com>
2022-03-18 08:42:56 -0500

    configuring views to retric acces only to logged in users

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render ,redirect
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import EntryForm, TopicForm
 
@@ -8,6 +9,7 @@ def index(request):
     """The home page for Learning Log"""
     return render(request, 'learning_logs/index.html') # render toma dos argumentos, el request y el nombre del template, esta vela por la carpeta base: templates, entoces el path real es: __dirname/learning_logs/templates/learning_logs/index.html
 
+@login_required # esto es un decorador, un decorador es una funcion que se ejecuta antes de que se ejecute la funcion que tiene abajo, esta sirve para alterar la funcion en cuestion, en este caso se usa @login_required para que solo los usuarios registrados puedan acceder a la funcion topics, si no estan logueados se redirecciona a la pagina de login
 def topics(request):
     """Show all topics."""
     topics = Topic.objects.order_by('date_added')
@@ -15,6 +17,7 @@ def topics(request):
     context = {'topics': topics} # context es un diccionario que se va a pasar a la plantilla para que pueda ser usado en la plantilla, las keys son los nombres de las variables que se van a usar en la plantilla, en este caso topics, y los values son los valores son los datos que necesitamos pasar a la plantilla.
     return render(request, 'learning_logs/topics.html', context) # render toma dos argumentos, el request y el nombre del template, esta vela por la carpeta base: templates, entoces el path real es: __dirname/learning_logs/templates/learning_logs/topics.html
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
@@ -23,6 +26,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -39,6 +43,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -59,6 +64,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """"Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
